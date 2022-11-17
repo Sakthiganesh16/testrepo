68572c7adc081c1041ee90d8ab8116904d19fd7b
Sakthiganesh16 <rjssganesh2000@gmail.com>
2022-11-17 13:11:49 +0530

    created a branch calc_prgm and edited calculator file

--- python_files/calculator.py
+++ python_files/calculator.py
@@ -4,7 +4,11 @@ def add(x,y):
     if x+y is not None:
         return x+y
     return "wrong input"
-
+def sub(x,y):
+    '''function to subtract 2 numbers'''
+    if x-y is not None:
+        return x-y
+    return "wrorng input"
 x=int(input("Enter the value of x : "))
 y=int(input("Enter the value of y : "))
-print(add(x,y))
+print(add(x,y),sub(x,y))
